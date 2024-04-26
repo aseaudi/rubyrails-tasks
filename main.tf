626627e659b90489238c4f1cbc04feba7c97cb97
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2024-04-27 01:00:22 +0300

    1

--- main.tf
+++ main.tf
@@ -13,12 +13,40 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "aseaudi-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = "RG_Simplex"
+}
+
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "aseaudi-subnet"
+  resource_group_name  = "RG_Simplex"
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "aseaudi-nic"
+  location            = "eastus"
+  resource_group_name = "RG_Simplex"
+
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
+}
+
 resource "azurerm_windows_virtual_machine" "main" {
   name                  = "aseaudi-vm"
   admin_username        = "azureuser"
   admin_password        = "pass"
   location              = "eastus"
   resource_group_name   = "RG_Simplex"
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
   size                  = "Standard_DS1_v2"
 
   os_disk {
@@ -33,6 +61,7 @@ resource "azurerm_windows_virtual_machine" "main" {
     sku       = "2022-datacenter-azure-edition"
     version   = "latest"
   }
+
 
 
 }
