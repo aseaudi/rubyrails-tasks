30db2e98eb529d0de34b55ea65538aa25c0f94bb
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2024-04-27 01:57:35 +0300

    1

--- main.tf
+++ main.tf
@@ -20,58 +20,8 @@ resource "azurerm_virtual_network" "my_terraform_network" {
   resource_group_name = "RG_Simplex"
 }
 
-resource "azurerm_subnet" "my_terraform_subnet" {
-  name                 = "aseaudi-subnet"
-  resource_group_name  = "RG_Simplex"
-  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "my_terraform_public_ip" {
-  name                = "aseaudi-public-ip"
-  location            = "eastus"
-  resource_group_name = "RG_Simplex"
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_network_interface" "my_terraform_nic" {
-  name                = "aseaudi-nic"
-  location            = "eastus"
-  resource_group_name = "RG_Simplex"
-
-  ip_configuration {
-    name                          = "my_nic_configuration"
-    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
-  }
-}
-
-resource "azurerm_windows_virtual_machine" "main" {
-  name                  = "aseaudi-vm"
-  admin_username        = "azureuser"
-  admin_password        = "mju7KO)%tgGF"
-  location              = "eastus"
-  resource_group_name   = "RG_Simplex"
-  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  size                  = "Standard_DS1_v2"
-
-  os_disk {
-    name                 = "myOsDisk"
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2022-datacenter-azure-edition"
-    version   = "latest"
-  }
 
 
-
-}
 
 
 
