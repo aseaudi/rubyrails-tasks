4f80141f9376504f4b1d0bc65c5ef55a7990eda2
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2024-04-27 00:57:26 +0300

    1

--- main.tf
+++ main.tf
@@ -13,6 +13,30 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_windows_virtual_machine" "main" {
+  name                  = "aseaudi-vm"
+  admin_username        = "azureuser"
+  admin_password        = "pass"
+  location              = "eastus"
+  resource_group_name   = "RG_Simplex"
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+
+
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "eastus"
