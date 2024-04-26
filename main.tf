37aa6c04788bf2c0d55fe893307981d124afb2c3
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2024-04-27 02:48:39 +0300

    1

--- main.tf
+++ main.tf
@@ -14,14 +14,113 @@ provider "azurerm" {
   features {}
 }
 
+# Create virtual network
 resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = "aseaudi-vnet"
+  name                = "${random_pet.prefix.id}-vnet"
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "${random_pet.prefix.id}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_postgresql_server" "example" {
+  name                = "postgresql-server-1"
   location            = "eastus"
+  resource_group_name = "RG_Simplex"
+
+  sku_name = "B_Gen5_2"
+
+  storage_mb                   = 5120
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+  auto_grow_enabled            = true
+
+  administrator_login          = "psqladmin"
+  administrator_login_password = "H@Sh1CoR3!"
+  version                      = "9.5"
+  ssl_enforcement_enabled      = true
+}
+
+resource "azurerm_postgresql_database" "example" {
+  name                = "exampledb"
+  resource_group_name = "RG_Simplex"
+  server_name         = azurerm_postgresql_server.example.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "example"
+  resource_group_name = "RG_Simplex"
+  location            = "eastus"
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "example"
+  resource_group_name = "RG_Simplex"
+  location            = "eastus"
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {}
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "example" {
+  name                        = "examplekeyvault"
+  location                    = "eastus"
+  resource_group_name         = "RG_Simplex"
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
+resource "azurerm_private_dns_zone" "example" {
+  name                = "mydomain.com"
   resource_group_name = "RG_Simplex"
 }
 
-
+resource "azurerm_private_dns_a_record" "example" {
+  name                = "test"
+  zone_name           = azurerm_private_dns_zone.example.name
+  resource_group_name = "RG_Simplex"
+  ttl                 = 300
+  records             = ["10.0.180.17"]
+}
 
 
 
