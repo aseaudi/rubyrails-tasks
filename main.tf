713bd330ba4d628bf6f5090ad2badf91b53339c4
Abdelmuhaimen Seaudi <abdelmuhaimen.seaudi@orange.com>
2024-04-27 07:24:35 +0300

    1

--- main.tf
+++ main.tf
@@ -20,14 +20,14 @@ resource "random_pet" "prefix" {}
 resource "azurerm_virtual_network" "my_terraform_network" {
   name                = "${random_pet.prefix.id}-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "eastus"
+  resource_group_name = "RG_Simplex"
 }
 
 # Create subnet
 resource "azurerm_subnet" "my_terraform_subnet" {
   name                 = "${random_pet.prefix.id}-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name = "RG_Simplex"
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
